819a496d9dc95cc3649d0154409a7e62668e2c68
yakhlafhoussam <yakhlafhoussam@gmail.com>
2026-09-15 12:17:04 +0100

    Refactor getData function to accept a path parameter for improved flexibility

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pandas as pd
 
-def getData():
-    df = pd.read_csv("sources/ma.csv")
+def getData(path):
+    df = pd.read_csv(path)
     return df
 
 def storeCities(cities, path):
     cities.to_csv(f"{path}cities.csv", index=False)
 
+def prepareUrl(lat, lng):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lng}&hourly=temperature_2m"
+    return url
+
+origine = "sources/ma.csv"
 bronze = "data/bronze/"
 silver = "data/silver/"
 gold = "data/gold/"
 
-cities = getData()
+cities = getData(origine)
 storeCities(cities, bronze)
